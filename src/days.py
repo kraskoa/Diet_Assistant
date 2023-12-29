376381b56fe7ec6f4da4e2f5d18d698e54c7f75c
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-12-29 16:26:21 +0100

    Updated day class

--- src/days.py
+++ src/days.py
@@ -1,16 +1,29 @@
-from meals import Meal
+# from meals import Meal
 
 
 class Day:
     """
     Class that represents a day of dieting, contains all of the day's meals
     """
-    def __init__(self, meals: list[Meal] = None):
+    none_dict = {
+        "Breakfast": None,
+        "Snack": None,
+        "Lunch": None,
+        "Teatime": None,
+        "Dinner": None
+    }
+
+    def __init__(self, number_of_meals: int = 3, meals: dict = none_dict):
         self._meals = meals
+        self._number_of_meals = number_of_meals
 
     @property
     def meals(self):
         return self._meals
+
+    @property
+    def number_of_meals(self):
+        return self._number_of_meals
 
     def calculate_total_calories(self) -> float:
         """
@@ -23,3 +36,17 @@ class Day:
         for meal in self._meals:
             daily_calories += meal.calculate_calories("portion")
         return daily_calories
+
+    def calculate_total_nutrients(self) -> dict:
+        """
+        Function that calculates the total nutritional values for the day
+
+        Returns:
+            dict: dictionary with total nutritional values for the day
+                  {"label": "quantity"}
+        """
+        daily_nutrients = {}
+        for meal in self._meals:
+            for nutrient in meal.nutrients:
+                daily_nutrients[nutrient["label"]] += nutrient["quantity"]
+        return daily_nutrients
---
+++ src/tests/test_days.py
@@ -0,0 +1,7 @@
+from days import Day
+
+
+def test_day_create():
+    day = Day(4)
+    assert day.meals["Breakfast"] is None
+    assert day.number_of_meals == 4
