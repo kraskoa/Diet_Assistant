c7c86804a3ae6dca9bd2d567aa39ef83e6d5e055
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-12-29 13:26:14 +0100

    Created function to read from secrets.json

---
+++ src/utils/load_secrets.py
@@ -0,0 +1,17 @@
+import json
+
+
+def load_secrets(path):
+    """
+    Function that reads data from secrets.json file
+
+    Args:
+        path (str): path to secrets.json file
+
+    Returns:
+        secrets (dict): a Python dictionary with data from secrets.json
+    """
+    secrets = {}
+    with open(path, "r") as file:
+        secrets = json.load(file)
+    return secrets
