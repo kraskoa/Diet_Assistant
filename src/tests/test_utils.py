f75119d7523da499f1eaa55e746e806de8cb40d6
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-12-30 23:43:21 +0100

    Refactor

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -2,12 +2,12 @@ from ..utils.get_meals import get_meals
 from ..utils.load_secrets import load_secrets
 
 
+secrets = load_secrets("secrets.json")
+
+
 def test_load_secrets():
     secrets = load_secrets("secrets.json")
     assert ["url", "app_id", "app_key"] == list(secrets.keys())
-
-
-secrets = load_secrets("secrets.json")
 
 
 def test_get_meals():
