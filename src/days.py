4cd14dfdccbc5bbd91b3e1c809086e6f59d8dc30
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-12-30 23:43:31 +0100

    Working on Day class

--- src/days.py
+++ src/days.py
@@ -1,21 +1,32 @@
-# from meals import Meal
+from .utils.get_meals import get_meals
+import random
+import sys
 
 
 class Day:
     """
     Class that represents a day of dieting, contains all of the day's meals
     """
-    none_dict = {
-        "Breakfast": None,
-        "Snack": None,
-        "Lunch": None,
-        "Teatime": None,
-        "Dinner": None
-    }
-
-    def __init__(self, number_of_meals: int = 3, meals: dict = none_dict):
-        self._meals = meals
+    def __init__(self, number_of_meals: int):
+        none_dict = {
+            "Breakfast": None,
+            "Snack": None,
+            "Lunch": None,
+            "Teatime": None,
+            "Dinner": None
+        }
         self._number_of_meals = number_of_meals
+        if self._number_of_meals == 3:
+            none_dict.pop("Snack")
+            none_dict.pop("Teatlime")
+            self._meals = none_dict
+        elif self._number_of_meals == 4:
+            none_dict.pop("Teatime")
+            self._meals = none_dict
+        elif self._number_of_meals == 5:
+            self._meals = none_dict
+        else:
+            raise ValueError("Number of meals has to be between 3 and 5")
 
     @property
     def meals(self):
@@ -33,7 +44,7 @@ class Day:
             float: total calories for the day
         """
         daily_calories = 0
-        for meal in self._meals:
+        for meal in self._meals.values():
             daily_calories += meal.calculate_calories("portion")
         return daily_calories
 
@@ -45,8 +56,35 @@ class Day:
             dict: dictionary with total nutritional values for the day
                   {"label": "quantity"}
         """
-        daily_nutrients = {}
-        for meal in self._meals:
-            for nutrient in meal.nutrients:
-                daily_nutrients[nutrient["label"]] += nutrient["quantity"]
+        daily_nutrients = {
+            "Fat": 0,
+            "Carbs": 0,
+            "Protein": 0,
+            "Fiber": 0,
+            "Sugars": 0
+        }
+        for meal in self._meals.values():
+            for nutrient in meal.calculate_nutrients("portion").items():
+                daily_nutrients[nutrient[0]] += nutrient[1]
         return daily_nutrients
+
+    def create_menu(self, secrets, max_calories: int = sys.maxsize, **kwargs):
+        for meal in self._meals.keys():
+            meals_list = get_meals(secrets, meal, **kwargs)
+            self._meals[meal] = random.choice(meals_list)
+
+
+if __name__ == "__main__":
+    d = Day(5)
+    secrets = {
+        "url": "https://api.edamam.com/api/recipes/v2?type=public&app_id={app_id}&app_key={app_key}&{diet}&{cuisineType}&mealType={mealType}",
+        "app_id": "350da553",
+        "app_key": "0e1f552796edd4134e2efadf38def7d1"
+    }
+    d.create_menu(secrets)
+    print(d.calculate_total_calories())
+    print(d.calculate_total_nutrients())
+    for item in d.meals.items():
+        print(f"{item[0]}: {item[1]}")
+
+# dodac pole Excluded, dishType do get_meals i do Meal
--- src/tests/test_days.py
+++ src/tests/test_days.py
@@ -1,7 +1,24 @@
+from ..utils.load_secrets import load_secrets
 from ..days import Day
+import pytest
+
+
+secrets = load_secrets("secrets.json")
 
 
 def test_day_create():
     day = Day(4)
-    assert day.meals["Breakfast"] is None
     assert day.number_of_meals == 4
+    assert len(day.meals) == 4
+    assert "Teatime" not in day.meals.keys()
+
+
+def test_day_wrong_value():
+    with pytest.raises(ValueError):
+        Day(6)
+
+
+def test_random_menu():
+    day = Day(5)
+    day.create_menu(secrets)
+    assert None not in day.meals.values()
