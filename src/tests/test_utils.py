f61b60a876a233026aba5f9943686c67062dd362
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-12-31 13:49:18 +0100

    Added new fields to get_meals()

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -15,3 +15,30 @@ def test_get_meals():
     assert len(list_of_meals_objects) == 20
     assert "Lunch".lower() in list_of_meals_objects[0].meal_type[0].split('/')
     assert "Asian".lower() in list_of_meals_objects[0].cuisine_type
+
+
+def test_get_meals_excluded():
+    list_of_meals_objects = get_meals(secrets, "Lunch", excluded="chicken")
+    all_ingredients = []
+    for meal in list_of_meals_objects:
+        all_ingredients.append(meal.ingredients)
+    assert "chicken" not in all_ingredients
+
+
+def test_get_meals_excluded_multiple():
+    list_of_meals_objects = get_meals(
+        secrets, "Lunch", excluded=["chicken", "butter"]
+        )
+    all_ingredients = []
+    for meal in list_of_meals_objects:
+        all_ingredients.append(meal.ingredients)
+    assert "chicken" not in all_ingredients
+    assert "butter" not in all_ingredients
+
+
+def test_get_meals_calories():
+    list_of_meals_objects = get_meals(secrets, "Lunch", calories=500)
+    all_meals_calories = []
+    for meal in list_of_meals_objects:
+        all_meals_calories.append(meal.calculate_calories("portion"))
+    assert max(all_meals_calories) <= 500
--- src/utils/get_meals.py
+++ src/utils/get_meals.py
@@ -1,5 +1,7 @@
 from ..meals import Meal
+
 import requests
+import random
 
 
 def get_meals(secrets: dict, meal_type: str, **kwargs) -> list:
@@ -14,17 +16,29 @@ def get_meals(secrets: dict, meal_type: str, **kwargs) -> list:
     Returns:
         list: a list of Meal objects
     """
-    defaults = {"diet": "", "cuisineType": ""}
+    defaults = {"diet": "", "cuisineType": "", "calories": ""}
+    # A list of all sensible dish types, without it the API returns many
+    # drinks/cocktails recipes which are unsuitable for the project
+    dish_types = ["Main course", "Sandwiches"]
+    exclusions = ""
     list_of_meals_objects = []
     for arg in kwargs.items():
-        defaults[arg[0]] = f"{arg[0]}={arg[1]}"
+        if arg[0] == "excluded":
+            for ingredient in arg[1]:
+                exclusions += f"excluded={ingredient}&"
+        else:
+            defaults[arg[0]] = f"{arg[0]}={arg[1]}"
 
+    chosen_dish_type = random.choice(dish_types)
     data = requests.get(secrets["url"].format(
         app_id=secrets["app_id"],
         app_key=secrets["app_key"],
         diet=defaults['diet'],
         cuisineType=defaults['cuisineType'],
-        mealType=meal_type
+        calories=defaults['calories'],
+        mealType=meal_type,
+        dishType=chosen_dish_type,
+        excluded=exclusions
     )).json()["hits"]
 
     for hit in data:
