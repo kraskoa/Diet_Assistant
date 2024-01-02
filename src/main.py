1b18dc61fe99faa9707dfb509465a5c53f676088
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-01-02 15:22:56 +0100

    main refactor and added fields for use to choose from

--- src/main.py
+++ src/main.py
@@ -1,5 +1,40 @@
 from .utils.create_secrets import check_if_secrets_exists
 
 
-if __name__ == "__main__":
+diets_list = [
+    "balanced",
+    "high-fiber",
+    "high_protein",
+    "low-carb",
+    "low-fat",
+    "low-sodium"
+]
+
+cuisine_types = [
+    "American",
+    "Asian",
+    "British",
+    "Caribbean",
+    "Central Europe",
+    "Chinese",
+    "Eastern Europe",
+    "French",
+    "Indian",
+    "Italian",
+    "Japanese",
+    "Kosher",
+    "Mediterranean",
+    "Mexican",
+    "Middle Eastern",
+    "Nordic",
+    "South American",
+    "South East Asian"
+]
+
+
+def main():
     check_if_secrets_exists()
+
+
+if __name__ == "__main__":
+    main()
