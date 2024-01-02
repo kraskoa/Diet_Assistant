92633cea10fc18d9df8adaa3ce284756a328a9f7
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-01-02 23:05:28 +0100

    Added new function and updated data for user

--- src/main.py
+++ src/main.py
@@ -1,39 +1,41 @@
 from .utils.create_secrets import check_if_secrets_exists
 
 
-diets_list = [
-    "balanced",
-    "high-fiber",
-    "high_protein",
-    "low-carb",
-    "low-fat",
-    "low-sodium"
-]
+available_diets = {
+    1: "balanced",
+    2: "high-fiber",
+    3: "high_protein",
+    4: "low-carb",
+    5: "low-fat",
+    6: "low-sodium"
+}
 
-cuisine_types = [
-    "American",
-    "Asian",
-    "British",
-    "Caribbean",
-    "Central Europe",
-    "Chinese",
-    "Eastern Europe",
-    "French",
-    "Indian",
-    "Italian",
-    "Japanese",
-    "Kosher",
-    "Mediterranean",
-    "Mexican",
-    "Middle Eastern",
-    "Nordic",
-    "South American",
-    "South East Asian"
-]
+cuisine_types = {
+    1: "American",
+    2: "Asian",
+    3: "British",
+    4: "Central Europe",
+    5: "Chinese",
+    6: "French",
+    7: "Indian",
+    8: "Italian",
+    9: "Japanese",
+    10: "Kosher",
+    11: "Mediterranean",
+    12: "Mexican",
+    13: "Middle Eastern",
+    14: "Nordic",
+    15: "South American"
+}
+
+
+def write_to_output_file(data):
+    with open("YourDiet.txt", "w") as output_file:
+        output_file.write(data)
 
 
 def main():
-    check_if_secrets_exists()
+    write_to_output_file("A\n\tB")
 
 
 if __name__ == "__main__":
